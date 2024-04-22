95f6b219c2d20b28f10b141241b053a2c585a877
Nikita Korotov <korn03@vse.cz>
2024-04-22 08:22:13 +0200

    working IME

--- src/InputField.js
+++ src/InputField.js
@@ -1,25 +1,28 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import hiragana from "./hiragana.json"; //hiragana.json is a file that contains the hiragana characters and their corresponding romaji
 import katagana from "./katagana.json"; //katagana.json is a file that contains the katagana characters and their corresponding romaji
+import * as wanakana from "wanakana"; //improve import later
 
 
 function InputField() {
     const [inputValue, setInputValue] = useState('');
-    const [potentialSymbol, setPotentialSymbol] = useState('');
+    const inputRef = useRef(null)
 
-    const handleKeyPress = (event) => {
-        const pressedKey = event.key;
+    useEffect(() => {
+        console.log(inputRef)
+        wanakana.bind(inputRef.current)
+      },[])
     
-    };
 
     return (
         <div>
             <input
                 type="text"
+                ref={inputRef}
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
-                onKeyDownCapture={(e) => handleKeyPress(e)}
+                //onKeyDownCapture={(e) => handleKeyPress(e)}
                 placeholder="Type here..."
             />
         </div>
