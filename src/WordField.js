7f36a9f5e48e57486388d57e25e21c6f6c1a0c56
Nikita Korotov <korn03@vse.cz>
2024-04-24 03:31:51 +0200

    working tooltip

--- src/WordField.js
+++ src/WordField.js
@@ -1,59 +1,54 @@
 import './App.css';
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setInputValue, setGuessWord, setAppMode } from './actions';
-//import words from './words.json'; //words library
 import * as wanakana from "wanakana";
 import Util from "./util";
-
 
 function WordField() {
     const guessWord = useSelector((state) => state.guessWord);
     const appMode = useSelector((state) => state.appMode);
     const dispatch = useDispatch();
-    const inputRef = useRef(null);
 
-    useEffect(() => {
-        //wanakana.bind(inputRef.current);
+    const splitWord = Util.splitWordToCharsObject(guessWord.jp);
 
-    }, []);
+    const [hoveredChar, setHoveredChar] = useState(null);
 
-    const handleChange = (e) => {
-        //dispatch(setInputValue(e.target.value));
-        //const typedValue = e.target.value;
-        //dispatch(setInputValue(typedValue));
-        dispatch(setGuessWord(Util.randomNewWord()));
-        dispatch(setInputValue(''));
+    const handleHover = (romaji) => {
+        setHoveredChar(romaji);
     };
-    const test = Util.splitWordToCharsObject(guessWord.jp);
-    console.log(test);
 
-    
+    const handleMouseLeave = () => {
+        setHoveredChar(null);
+    };
+
     const renderWord = () => {
-        if (appMode === 'r2k') {
-            return (
-                <h1>{wanakana.toRomaji(guessWord.jp.wd)}</h1>
-            );
-        } 
-        if (appMode === 'k2r'){
-            return (
-                <h1>{wanakana.toKana(guessWord.jp.wd)}</h1>
-            );
-        }
-    }
-    
+        const charsArray = appMode === 'r2k' ? splitWord.rmj : splitWord.wd;
+
+        return charsArray.map((char, index) => (
+            <span
+                className='word-part'
+                key={index}
+                onMouseEnter={() => handleHover(appMode === 'r2k' ? char : wanakana.toRomaji(char))}
+                onMouseLeave={handleMouseLeave}
+            >
+                {char}
+                {hoveredChar === (appMode === 'r2k' ? char : wanakana.toRomaji(char)) && (
+                    <span className="tooltip active">
+                        {hoveredChar}
+                    </span>
+                )}
+            </span>
+        ));
+    };
+
     return (
         <div className='word-field'>
-            {
-                //wanakana.tokenize(wanakana.toRomaji(guessWord.jp.wd))
-                renderWord()
-                /*
-                chunkedWord.map((token, index) => (
-                    <span key={index}>{token}</span>
-                  ))
-                  */
-            }
-            <button onClick={handleChange}>
+            {renderWord()}
+            <button onClick={() => {
+                dispatch(setGuessWord(Util.randomNewWord()));
+                dispatch(setInputValue(''));
+            }}>
                 New
             </button>
         </div>
