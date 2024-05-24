bbeb059e32642295e9a78566354646e29d1c9b6c
Nikita Korotov <korn03@vse.cz>
2024-05-24 16:38:25 +0200

    consent-banner component, google analytics remake (strictly to count page views)

---
+++ src/App.tsx
@@ -0,0 +1,82 @@
+import "./styles/App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReactGA from 'react-ga4';
+//redux
+import { Provider } from "react-redux";
+import Store from "./redux/store";
+import Header from "./components/Header";
+// components
+import Layout from "./components/Layout";
+import InputField from "./components/InputField";
+import WordField from "./components/WordField";
+import FAQPage from "./components/FAQPage";
+import Footer from "./components/Footer";
+import ConsentBanner from './components/ConsentBanner';
+
+const TRACKING_ID = 'G-YTBJVQT22P';
+
+function App() {
+  const [hasConsent, setHasConsent] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const consent = localStorage.getItem('consent');
+    if (consent !== null) {
+      const consentValue = consent === 'true';
+      setHasConsent(consentValue);
+      if (consentValue) {
+        ReactGA.initialize(TRACKING_ID);
+      }
+    }
+  }, []);
+
+  const handleConsent = (consent: boolean) => {
+    localStorage.setItem('consent', String(consent));
+    setHasConsent(consent);
+    if (consent) {
+      ReactGA.initialize(TRACKING_ID);
+    }
+  };
+  
+
+  return (
+    <Router basename="romaji2kanji">
+      <Provider store={Store}>
+        <div className="App">
+          <Header />
+          {hasConsent === null && <ConsentBanner onConsent={handleConsent} />}
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/faq" element={<FAQPage />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Provider>
+    </Router>
+  );
+}
+
+const HomePage = () => {
+  useEffect(() => {
+    const consent = localStorage.getItem('consent');
+    if (consent === 'true') {
+      ReactGA.send({ 
+        hitType: 'pageview', 
+        page: window.location.pathname,
+        title: "app"
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="main-wrapper">
+        <WordField />
+        <InputField />
+        <Layout />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/ConsentBanner.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import '../styles/ConsentBanner.css';
+
+interface ConsentBannerProps {
+  onConsent: (consent: boolean) => void;
+}
+
+const ConsentBanner: React.FC<ConsentBannerProps> = ({ onConsent }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleConsent = (consent: boolean) => {
+    setIsVisible(false);
+    onConsent(consent);
+  };
+
+  if (!isVisible) return null;
+
+  return (
+    <div className='consent-banner'>
+      <p>We use cookies to improve your experience. Do you consent to the use of cookies?</p>
+      <div className='consent-buttons-container'>
+      <button onClick={() => handleConsent(true)}>Yes</button>
+      <button onClick={() => handleConsent(false)}>No</button>
+      </div>
+    </div>
+  );
+};
+
+export default ConsentBanner;
